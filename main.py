f9161ce546d554fc1aa71599c75e9a7c5acdf127
Seynro <oseymur0@gmail.com>
2023-05-27 01:11:33 +0400

    new_meth
    added name and columns creation in new tables

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ bot = telebot.TeleBot('5541614984:AAFU3OavEq8sbn-4lKCcS0J9EeP7L16QSnc')
 
 from functions import creating_new_table
 
+keys = ['name', 'num_colomns', 'colomn']
+table_dict = dict.fromkeys(keys)
+colomns_list = []
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    global table_dict
+    global colomns_list
     if message.text == "/start":
         
         keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
@@ -21,13 +27,40 @@ def get_text_messages(message):
     
     if '/reg' in message.text:
         bot.send_message(message.from_user.id, "Название: ")
-        bot.register_next_step_handler(message, creating_new_table)
+        bot.register_next_step_handler(message, name)
+        
+        bot.send_message(message.from_user.id, "Количество столбцов: ")
+        bot.register_next_step_handler(message, num_colomns)
+        num = 3
+        i = 0
+        while not i == num:
+            mes = 'Название ' + str(i) + 'столбца'
+            bot.send_message(message.from_user.id, mes)
+            bot.register_next_step_handler(message, colomns)
+            i += 1
+        table_dict['colomn'] = colomns_list
+        print(table_dict)
+        #bot.register_next_step_handler(message, creating_new_table)
 
     if '/in' in message.text:
         bot.send_message(message.from_user.id, "Открываю...")
     
+###################################
+def name(message):
+    global table_dict
+    text_buffer = message.text
+    table_dict['name'] = text_buffer
 
+def num_colomns(message):
+    global table_dict
+    text_buffer = message.text
+    table_dict['num_colomns'] = text_buffer
 
+def colomns(message):
+    global table_dict
+    global colomns_list
+    text_buffer = message.text
+    colomns_list.append(text_buffer)
 ####################################
 
 @bot.callback_query_handler(func=lambda call: True)
