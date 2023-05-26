26d6858170ea404f089343c13fe33e51b43d0ae3
Seynro <oseymur0@gmail.com>
2023-05-26 17:35:05 +0400

    Basic responses
    Basic responses in telegram

--- main.py
+++ main.py
@@ -8,14 +8,21 @@ def get_text_messages(message):
     if message.text == "/start":
         
         keyboard = types.InlineKeyboardMarkup(); #наша клавиатура
-        key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes'); #кнопка «Да»
+        key_yes = types.InlineKeyboardButton(text='Создать новую таблицу', callback_data='new_table'); #кнопка
         keyboard.add(key_yes); #добавляем кнопку в клавиатуру
-        key_no= types.InlineKeyboardButton(text='Нет', callback_data='no');
+        key_no= types.InlineKeyboardButton(text='Войти в существующую таблицу', callback_data='old_table');
         keyboard.add(key_no);
         bot.send_message(message.from_user.id, "Привет, это бот помогающий контролировать твою локальную SQL таблицу. Выбери: ", reply_markup=keyboard)
         
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    
+    if '/reg' in message.text:
+        bot.send_message(message.from_user.id, "Создаю таблицу...")
+
+    if '/in' in message.text:
+        bot.send_message(message.from_user.id, "Открываю...")
+    
     elif message.text == "/help":
         bot.send_message(message.from_user.id, "Напиши привет")
 
@@ -24,11 +31,13 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        .... #код сохранения данных, или их обработки
-        bot.send_message(call.message.chat.id, 'Запомню : )');
-    elif call.data == "no":
-         ... #переспрашиваем
+    if call.data == "new_table":
+        #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Введите название новой таблицы с использованием /reg: ')
+
+    elif call.data == "old_table":
+        #код сохранения данных, или их обработки
+        bot.send_message(call.message.chat.id, 'Введите название нужной таблицы с использованием /in: ');
 
 ####################################
 
